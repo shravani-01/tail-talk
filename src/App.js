e68ac050106d3bc100ea22fd56f705e26c6eea7f
Shravani Hariprasad <shariprasad3296@MacBook-Pro-2.local>
2024-04-24 11:50:02 -0700

    updated files for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { default as React } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import AI from './components/AI';
 import Analysis from './components/Analysis';
 import Data from './components/Data';
